8a50cf401995150103e921f86f9eeed4f64ec5dc
Ezekiel Craig <ecrai30@wgu.edu>
2024-06-05 21:31:01 -0500

    Updated Files

--- app/src/main/java/com/example/d308vacationsapp/UI/ExcursionDetails.java
+++ app/src/main/java/com/example/d308vacationsapp/UI/ExcursionDetails.java
@@ -1,9 +1,6 @@
 package com.example.d308vacationsapp.UI;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,22 +10,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.d308vacationsapp.R;
 
-public class MainActivity extends AppCompatActivity {
+public class ExcursionDetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        Button button=findViewById(R.id.button2);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(MainActivity.this, VacationList.class);
-                intent.putExtra("test", "Information sent");
-                startActivity(intent);
-            }
-        });
+        setContentView(R.layout.activity_excursion_details);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/example/d308vacationsapp/UI/VacationDetails.java
+++ app/src/main/java/com/example/d308vacationsapp/UI/VacationDetails.java
@@ -2,10 +2,7 @@ package com.example.d308vacationsapp.UI;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,43 +13,26 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.d308vacationsapp.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class VacationList extends AppCompatActivity {
+public class VacationDetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_vacation_list);
-        FloatingActionButton fab=findViewById(R.id.floatingActionButton);
+        setContentView(R.layout.activity_vacation_details);
+        FloatingActionButton fab=findViewById(R.id.floatingActionButton2);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(VacationList.this, VacationDetails.class);
+                Intent intent=new Intent(VacationDetails.this, ExcursionDetails.class);
                 startActivity(intent);
             }
         });
-
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-    public boolean onCreateOptionsMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.menu_vacation_list,menu);
-        return true;
-    }
-
-    public boolean onOptionsItemSelected(MenuItem item){
-        if(item.getItemId()==R.id.sample){
-            Toast.makeText(VacationList.this, "put in sample data", Toast.LENGTH_SHORT).show();
-            return true;
-        }
-        if(item.getItemId()==android.R.id.home){
-            this.finish();
-            return true;
-        }
-        return true;
     }
 }
